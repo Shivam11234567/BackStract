95a9133ea5495d5c0a8ff0234816d11d1b5dd4bc
Shivam11234567 <shivamtesting7@gmail.com>
2025-04-07 15:28:48 +0530

    Add GitHub Actions for Selenium tests

--- conftest.py
+++ conftest.py
@@ -1,15 +1,31 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import os
 
-@pytest.fixture(scope="function")
-def driver():
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Chrome(options=options)
+
+@pytest.fixture
+def driver(request):
+    use_remote = request.config.getoption("--driver") == "Remote"
+
+    if use_remote:
+        options = Options()
+        options.add_argument('--headless')  # Optional: headless for CI
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+
+        driver = webdriver.Remote(
+            command_executor=request.config.getoption("--remote-url"),
+            options=options
+        )
+    else:
+        driver = webdriver.Chrome()
+
+    driver.maximize_window()
     yield driver
     driver.quit()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--driver", action="store", default="Local")
+    parser.addoption("--remote-url", action="store", default="http://localhost:4444/wd/hub")
