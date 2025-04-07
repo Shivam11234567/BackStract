397a0f60a73f008ac49da9f0ebf1dd5f8ef477d0
Shivam11234567 <shivamtesting7@gmail.com>
2025-04-07 15:54:23 +0530

    Set up GitHub Actions for Selenium tests

--- conftest.py
+++ conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-import os
 
 
 @pytest.fixture
@@ -10,9 +9,9 @@ def driver(request):
 
     if use_remote:
         options = Options()
-        options.add_argument('--headless')  # Optional: headless for CI
-        options.add_argument('--no-sandbox')
-        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
 
         driver = webdriver.Remote(
             command_executor=request.config.getoption("--remote-url"),
